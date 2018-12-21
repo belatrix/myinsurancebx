1ccb69a1386d150a3b25b8510f722f9effb209df
Sergio Infante <raulsergio9@gmail.com>
2018-12-20 21:42:27 -0500

    Initial separation of settings.py file into local (for development) and production files

---
+++ MyInsurance/settings/local.py
@@ -0,0 +1,6 @@
+"""
+Set this environment variable
+DJANGO_SETTINGS_MODULE=MyInsurance.settings.local
+"""
+
+from .base import *  # noqa: F403
---
+++ MyInsurance/settings/production.py
@@ -0,0 +1,6 @@
+"""
+Set this environment variable
+DJANGO_SETTINGS_MODULE=MyInsurance.settings.production
+"""
+
+from .base import *  # noqa: F403
