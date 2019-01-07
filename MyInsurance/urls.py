c53696f5dcef644bb10d8dac5272a70a8d64da78
Sergio Infante <raulsergio9@gmail.com>
2019-01-07 16:09:24 -0300

    Add coreapi documentation

--- MyInsurance/urls.py
+++ MyInsurance/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from rest_framework.documentation import include_docs_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('users.urls')),
+    path('docs/', include_docs_urls(title='My Insurance Company')),
 ]
