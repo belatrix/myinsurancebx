10f99c17e299827fd4ff986538d761808694f83e
Sergio Infante <raulsergio9@gmail.com>
2019-01-07 13:24:18 -0300

    Add boolean fields for user types and remove role model

--- users/admin.py
+++ users/admin.py
@@ -1,27 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Role, User
-
-
-class RoleAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+from .models import User
 
 
 class UserCustomAdmin(UserAdmin):
-    list_display = ('username', 'is_staff', 'is_superuser', 'role')
+    list_display = ('username', 'is_superuser', 'is_inspector', 'is_from_insurance', 'is_from_auto_repair_shop')
     search_fields = ['username']
     fieldsets = (
         (None, {'fields': ('username', 'email', 'password')}),
         ('Personal info', {'fields': ('first_name',
-                                      'last_name',
-                                      'role')}),
+                                      'last_name')}),
         ('Permissions', {'fields': ('is_superuser',
                                     'is_staff',
+                                    'is_inspector',
+                                    'is_from_insurance',
+                                    'is_from_auto_repair_shop',
                                     'groups',
                                     'user_permissions')}),
         ('History', {'fields': ('date_joined', 'last_login')})
     )
 
 
-admin.site.register(Role)
 admin.site.register(User, UserCustomAdmin)
--- users/models.py
+++ users/models.py
@@ -2,16 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class Role(models.Model):
-    name = models.CharField(max_length=100)
-
-    class Meta(object):
-        ordering = ['name']
-
-    def __str__(self):
-        return self.name
-
-
 class User(AbstractUser):
-    role = models.ForeignKey(Role, blank=True, null=True, on_delete=models.CASCADE)
+    is_inspector = models.BooleanField(default=False)
+    is_from_insurance = models.BooleanField(default=False)
+    is_from_auto_repair_shop = models.BooleanField(default=False)
     pass
