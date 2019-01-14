764d5704d04484f2394e09e979255d28fa73b62e
Sergio Infante <raulsergio9@gmail.com>
2019-01-14 18:00:40 -0500

    Add static url settings for media files

--- MyInsurance/urls.py
+++ MyInsurance/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+
 
 from rest_framework.documentation import include_docs_urls
 
@@ -25,4 +28,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('users.urls')),
     path('docs/', include_docs_urls(title='My Insurance Company')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
