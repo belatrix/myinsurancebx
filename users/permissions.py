2a250db44655ae7aa4d7be6364eb69b603de9e72
Sergio Infante <raulsergio9@gmail.com>
2019-01-29 07:01:09 -0500

    Add insurance permission

--- users/permissions.py
+++ users/permissions.py
@@ -8,6 +8,13 @@ class IsInspectorOrStaff(permissions.BasePermission):
         return request.user and (request.user.is_inspector or request.user.is_staff)
 
 
+class IsInsuranceOrStaff(permissions.BasePermission):
+    message = "Insurance restricted or Staff member"
+
+    def has_permission(self, request, view):
+        return request.user and (request.user.is_from_insurance or request.user.is_staff)
+
+
 class IsStaff(permissions.BasePermission):
     message = "Staff restricted"
 
