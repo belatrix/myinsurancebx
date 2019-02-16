9e10300cd02e166b4b96b8745c118dc95c2030f0
Sergio Infante <raulsergio9@gmail.com>
2019-02-16 13:46:47 -0500

    Add auto repair shop assign endpoint
    Signed-off-by: Sergio Infante <raulsergio9@gmail.com>

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import order_bulk_deletion, order_priority_change
+from .views import order_bulk_deletion, order_priority_change, auto_repair_shop_assign
 from .views import order_creation, order_detail, order_list, order_update, upload_file, order_status_list
 from .views import order_status_change, auto_repair_shop_list, order_budget_update
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('<int:order_id>/budget/change/', order_budget_update, name='order_budget_update'),
     path('<int:order_id>/detail/', order_detail, name='order_detail'),
     path('<int:order_id>/status/change/to/<int:status_id>', order_status_change, name='order_status_change'),
+    path('<int:order_id>/repairshop/change/to/<int:repairshop_id>', auto_repair_shop_assign, name='repairshop_assign'),
     path('<int:order_id>/priority/change/to/<int:priority>', order_priority_change, name='order_priority_change'),
     path('<int:order_id>/update/', order_update, name='order_update'),
     path('<int:order_id>/upload/file/', upload_file, name='upload_file'),
